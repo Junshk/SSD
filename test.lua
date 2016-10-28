a86ba179d2b426fc01ea1cb33dbebf4aa46a04f7
Junshk <snu11ee@snu.ac.kr>
2016-10-28 23:30:26 +0900

    modify test code / nms loop

--- test.lua
+++ test.lua
@@ -6,19 +6,43 @@ require 'pascal'
 require 'prior_box'
 
 torch.setdefaulttensortype('torch.FloatTensor')
-
+--local real_box_redefine =real_box_ratio:t():view(1,4,20097):copy()
 --local matio = require 'matio'
-local test_txt
-local test_list = {}
+--local test_txt
+--local test_list = {}
 local softmax = cudnn.LogSoftMax():cuda()
 softmax:evaluate()
 
 --------------------------------------------
+function write_txt(result,folder,class_num)
+  assert(folder~=nil, 'need folder name to write txt file')
+   -- write in filename txt 
+      if paths.dirp(folder) ==false then os.execute('mkdir '..folder) end
+
+--      for class_num = 1, 20 do
+
+        local class_box , class_score = result.box, result.score             
+        local write_result = io.open(folder..'/'..'comp3_det_test_'..num2class(class_num)..'.txt',"a")
+    --      for image_iter = 1, #class_result do
+          --   local image_result = class_result[image_iter]         
+            local image_name = string.sub(result.image_name,-11,-1)
+
+            for iter2 = 1, class_box:size(1) do
+            --local box = result.box[{iter2}]
+            local box = class_box[{{iter2}}]:squeeze()
+            write_result:write(image_name,' ',result.score[{iter2}],' ',box[1],' ',box[2],' ',box[3],' ',box[4],'\n' )
+
+            end
+      --    end
+        
+--      end
+  end
+
 ---------------------------------------------
 
 function test(net,list,folder)
-local i1 = os.time()
-net:evaluate()
+  local i1 = os.time()
+  net:evaluate()
 
   print('testing..')
   local list = list or test_list
@@ -28,15 +52,16 @@ net:evaluate()
   
   local result_vector = torch.Tensor(#list,25,20097)
 
-  local batch = 13
+  local batch = 12
   local iter = 1
 
 -- forward
   while iter <= #list do
    
   local start_iter , end_iter = iter, math.min(iter +batch -1,#list)
-  local input_tensor = torch.Tensor(end_iter-start_iter+1,3,500,500)
-  
+  local n = end_iter - start_iter +1
+  local input_tensor = torch.Tensor(n,3,500,500)
+          -- input --
     for iter = start_iter ,end_iter do
     local imagename = list[iter].image_name
 
@@ -46,15 +71,54 @@ net:evaluate()
     input_tensor[{{iter-start_iter+1}}] = image.scale(img,500,500)
   
     end
- 
-  local n =net:forward(input_tensor:cuda())
-  result_vector[{{start_iter,end_iter}}] = n:float():squeeze()
-
-  input_tensor =nil; collectgarbage();
+          -----------
+          --forward--
+  local output =net:forward(input_tensor:cuda()):float()
+ -- result_vector[{{start_iter,end_iter}}] = output:float():squeeze()
   
-  iter = end_iter + 1
+  local conf_before_softmax = output[{{},{1,21}}]:transpose(2,3):reshape(n*20097,21)
+  local conf = softmax:forward(conf_before_softmax:cuda()):view(n,20097,21):exp():float()
+  
+  local refined_box = output[{{},{22,25}}]
+
+  refined_box = refined_box + real_box_ratio:view(1,4,20097):expand(n,4,20097)
+  refined_box =refined_box:transpose(2,3)
+  local score, recognition = torch.max(conf,3)
+  -- nms
+  for iter_image = 1, n do
+    
+    local image_name = list[iter_image+start_iter-1].image_name
+    local size = image.load(image_name..'.jpg'):size()
+    for iter_class =1, 20 do
+    local res = {}
+    local index = torch.eq(recognition[{iter_image,{},{}}],iter_class)
+   -- print(index:size(),refined_box[iter_image]:size())
+    local detection_box = refined_box[iter_image]
+-- print(detection_box:size(),index:type())   
+    detection_box =detection_box[index:expandAs(detection_box)]
+ 
+    detection_box = detection_box:view(-1,4)
+    local detection_score = score[iter_image][index]:view(-1)
+    res.image_name = image_name
+    
+    --nms
+    res.box, res.score = nms(detection_box,0.45,detection_score,size)
+
+    write_txt(res, folder,iter_class)
+    
+    end
+
   end
 
+
+
+
+
+  input_tensor =nil; collectgarbage();
+  result = {}
+  iter = end_iter + 1
+  end
+--[[
   net:float()
 
 -- softmax
@@ -105,7 +169,8 @@ net:evaluate()
    
   end
 
-local i4 =os.time()
+  local i4 =os.time()
+  
   if folder ==nil then
   return result
   else -- write in filename txt 
@@ -128,8 +193,10 @@ local i4 =os.time()
         
       end
   end
-
-print('test time : ',i2-i1,i3-i2,i4-i3)
+]]--
+--  print('test time per a image : SSD',(i2-i1)/#list,'SOFTMAX',(i3-i2)/#list,'NMS',(i4-i3)/#list)
+local i5 = os.time()
+print('tot test time per an image',(i5-i1)/#list)
   net:training()
   net:cuda()
 
