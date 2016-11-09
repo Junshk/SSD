e096fd29667ac8ebf209f84cc06d1a2b4d2b8a10
Junshk <snu11ee@snu.ac.kr>
2016-11-09 16:43:52 +0900

    plot

--- plot_map.m
+++ plot_map.m
@@ -1,25 +1,31 @@
+
+function plot_map(result_folder)
+
 addpath('VOCdevkit/')
 addpath('VOCdevkit/VOCcode/')
 
 VOCinit;
 
 %validation test and map plot
-folder = dir('validation/val*');
+folder = dir([result_folder '/val*']);
+VOCopts
 tic;
 for j = 1:length(folder)
     folder_ = folder(j).name ;
-    VOCopts.detrespath = 'validation/%s/comp3_det_test_%s.txt' ;
-
-    for i = 1:1
+    VOCopts.detrespath = [result_folder '/%s/comp3_det_test_%s.txt' ];
+    VOCopts.imgsetpath = [result_folder '/' folder_ '/%s.txt'];
+    VOCopts.testset = 'test';
+        for i = 1:20
 
         class = VOCopts.classes{i};
-        VOCevaldet(VOCopts, folder_,class,true )
+        VOCevaldet(VOCopts, folder_,class,true );
+        viewdet(VOCopts,folder_ ,class,true)
     end
 end
 
 
 
-
+end
 %VOCopts%
 % testset test
 %eval_det('test/')
