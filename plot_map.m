12c9981966f526367517b7a83f4b64cf6f7e91a1
Junshk <snu11ee@snu.ac.kr>
2016-12-23 20:41:06 +0900

    plot map

--- plot_map.m
+++ plot_map.m
@@ -1,26 +1,43 @@
 
 function plot_map(result_folder)
-
+result_folder
 addpath('VOCdevkit/')
 addpath('VOCdevkit/VOCcode/')
 
 VOCinit;
+if strcmp(result_folder , 'test2012')==1
+  VOCopts.dataset = 'VOC2012_test'
+end
+if strcmp(result_folder , 'test2007')==1
+  VOCopts.dataset = 'VOC2007'
+end
+if strcmp(result_folder , 'testValid')==1
+    VOCopts.testset = 'val';
+end
 
 %validation test and map plot
 %folder = dir([result_folder '/val*']);
 folder = result_folder;
-    VOCopts.detrespath = [result_folder '/comp3_det_test_%s.txt' ];
+    VOCopts.detrespath = ['%s/comp3_det_test_%s.txt' ];
     VOCopts.imgsetpath = [result_folder  '/%s.txt'];
-    VOCopts.testset = 'test';
 VOCopts
 tic;
 
         for i = 1:20
 
         class = VOCopts.classes{i};
-        VOCevaldet(VOCopts, '/',class,true );
-        viewdet(VOCopts,'/' ,class,false)
+        VOCevaldet(VOCopts, result_folder,class,true );
+        saveas(gcf,class,'jpg')
+        %viewdet(VOCopts,result_folder ,class,false)
     end
+
+
+
+
+
+
+
+
 end
 
 
